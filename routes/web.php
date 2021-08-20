fa4a69a24b901dc8fb4c92819de5af0e58f237f5
vanis feutio <vanisfeutio6@gmail.com>
2021-08-20 17:49:27 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/* ADMINISTRATION */
+    Route::group(['as' => 'admin::', 'prefix' => 'admin'], function() {
+
+        Route::get('/product/list', 'Admin\AdminProductController@index');
+        Route::get('/product/create', 'Admin\AdminProductController@createNew');
+        Route::post('/product/create', 'Admin\AdminProductController@create')->name("create.product");
+        Route::get('/product/update{id}', 'Admin\AdminProductController@updateNew')->name('update.product');
+        Route::post('/product/update', 'Admin\AdminProductController@update')->name("updated.product");
+        Route::get('/product/delete{id}', 'Admin\AdminProductController@delete')->name("delete.product");
+        Route::get('/product/detail{id}', 'Admin\AdminProductController@detail')->name("view.product");
+    });
 
 
 
